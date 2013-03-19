39075f67e202061fb2e269f3b9eac3f70f8d4851
Bharath Santosh <bsant99@gmail.com>
2013-03-19 09:56:21 -0400

    Renamed handshake to more appropriate version handshake code

--- Network/SSH/Client.hs
+++ Network/SSH/Client.hs
@@ -13,7 +13,7 @@ import Control.Proxy((>->))
 import qualified Control.Proxy as P
 import Network.SSH.Transport
 
-data SSHMode = HandShake | KeySwap
+data SSHMode = VersionHandShake | KeySwap
 
 data SSHContext = SSHContext {
     sshMode :: SSHMode
@@ -38,59 +38,59 @@ sshClient hostname port = withSocketsDo $ do
     setSocketOption sock KeepAlive 1
     connect sock (addrAddress serveraddr)
 
-    let initSSHContext = SSHContext { sshMode = HandShake }
+    let initSSHContext = SSHContext { sshMode = VersionHandShake }
     P.runProxy $ socketHandler sock 4096 >-> sshClientProxy initSSHContext
 
     sClose sock
 
-sshHandShake :: String
-sshHandShake  = sshVersion ++ "\r\n"
+sshVersionHandShake :: String
+sshVersionHandShake  = sshVersion ++ "\r\n"
 
 sshClientProxy :: (P.Proxy p) => SSHContext -> () -> P.Client p SocketContext SocketContext IO ()
 sshClientProxy context () = P.runIdentityP (clientLoop context) where
     clientLoop context =  case sshMode context of
-                                HandShake -> handShake
+                                VersionHandShake -> versionHandShake
                                 _ -> clientMain
         where
-            handShake = do
-                let handShakeDatum = return $ BS.pack sshHandShake
+            versionHandShake = do
+                let versionHandShakeDatum = return $ BS.pack sshVersion
                 let initSocketContext = SocketContext { socketReadDatum = [],
-                                                        socketWriteDatum = [handShakeDatum],
+                                                        socketWriteDatum = [versionHandShakeDatum],
                                                         socketStream = Write,
                                                         socketPacketize = NoPackets }
                 isc <- P.request initSocketContext
-                rem_datum <- takeFullSplashPage isc
+                rem_datum <- preVersionMsgs isc
                 P.request $ isc { socketReadDatum = rem_datum, socketStream = Rewind}
                 clientLoop $ context { sshMode = KeySwap }
                 where
-                    takeFullSplashPage sc = do
+                    preVersionMsgs sc = do
                         sc2 <- P.request $ (sc :: SocketContext) { socketStream = Read }
-                        spashBS <- lift $ head $ socketReadDatum (sc2 :: SocketContext)
-                        rem_datum <- lift $ printSSHSplashPage spashBS
+                        msgBS <- lift $ head $ socketReadDatum (sc2 :: SocketContext)
+                        rem_datum <- lift $ printSSHPreVersionMsg msgBS
                         case rem_datum of
-                            [] -> takeFullSplashPage $ sc2 { socketReadDatum = [] }
+                            [] -> preVersionMsgs $ sc2 { socketReadDatum = [] }
                             _ -> return rem_datum
-                    printSSHSplashPage :: BS.ByteString -> IO SocketDatum
-                    printSSHSplashPage bs = do
-                        let (splash,handShakeBegin) = case (BS.take 4 bs) == BS.pack "SSH-" of
+                    printSSHPreVersionMsg :: BS.ByteString -> IO SocketDatum
+                    printSSHPreVersionMsg bs = do
+                        let (preMsg,versionHandShakeBegin) = case (BS.take 4 bs) == BS.pack "SSH-" of
                                                         True  -> (BS.empty,bs)
                                                         False -> BS.breakSubstring (BS.pack "\r\nSSH-") bs
-                        case (BS.length handShakeBegin) == (BS.length bs) of
-                            True -> BS.putStr "Server Handshake: "
-                                    >> BS.putStrLn handShakeBegin
+                        case (BS.length versionHandShakeBegin) == (BS.length bs) of
+                            True -> BS.putStr "Server Version Handshake: "
+                                    >> BS.putStrLn versionHandShakeBegin
                                     >> return [return bs]
-                            False -> case (BS.length splash) == (BS.length bs)  of
-                                        True -> BS.putStrLn "Server Splash: "
-                                                >> BS.putStr splash
+                            False -> case (BS.length preMsg) == (BS.length bs)  of
+                                        True -> BS.putStrLn "Server Pre Start Messages: "
+                                                >> BS.putStr preMsg
                                                 >> return []
                                         False -> do
-                                                    BS.putStrLn splash
-                                                    let (serverHandShake,nonHandShake) = BS.breakSubstring (BS.pack "\r\n") (BS.drop 2 handShakeBegin)
-                                                    case (BS.length serverHandShake) == ((BS.length handShakeBegin) - 2) of
-                                                        True  -> BS.putStr "Server Handshake: "
-                                                                 >> BS.putStrLn serverHandShake
-                                                                 >> return [return (BS.drop 2 nonHandShake)]
-                                                        False -> error "Incorrect Server Handshake"
+                                                    BS.putStrLn preMsg
+                                                    let (serverVersionHandShake,nonVersionHandShake) = BS.breakSubstring (BS.pack "\r\n") (BS.drop 2 versionHandShakeBegin)
+                                                    case (BS.length serverVersionHandShake) == ((BS.length versionHandShakeBegin) - 2) of
+                                                        True  -> BS.putStr "Server Version Handshake: "
+                                                                 >> BS.putStrLn serverVersionHandShake
+                                                                 >> return [return (BS.drop 2 nonVersionHandShake)]
+                                                        False -> error "Incorrect Server Version  Handshake"
 
             clientMain = do
                 return ()

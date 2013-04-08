1b77fb29baab21a7b7e3ea97f7f410e8a673f36b
Bharath Santosh <bsant99@gmail.com>
2013-04-08 00:26:41 -0400

    Removed binary dependency

--- Network/SSH/Transport.hs
+++ Network/SSH/Transport.hs
@@ -4,6 +4,7 @@ import Data.ByteString.Char8 as BS
 sshVersion :: String
 sshVersion = "SSH-2.0-HaskellSSHClient_0.1"
 
+
 supportedKeyExchangeMethods :: [String]
 supportedKeyExchangeMethods =
     ["diffie-hellman-group1-sha1",
@@ -22,6 +23,20 @@ supportedMACAlgorithms = [("hmac-sha1")]
 supportedCompressionMethods :: [String]
 supportedCompressionMethods = ["none"]
 
+
+data PacketContext = PacketContext {
+	compressionType :: String,
+	encryptionType :: String,
+	macType :: String
+}
+
+--data SSHPacket = SSHPacket {
+--	packet_lenght :: {-# UNPACK #-} !Word32,
+--	padding_length :: # UNPACK # !Word8,
+--	payload ::  [!Word8],
+--	random_padding ::  [!Word8],
+--	mac ::  [!Word8]
+--}
 
 unpackPacket :: BS.ByteString -> String
 unpackPacket bs = undefined
